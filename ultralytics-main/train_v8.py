35d1f73c0c83be4eee63c21e12ff6c4ea9681df5
pengfei_mac <panda_du@msn.com>
2025-06-28 22:46:24 +0800

    修改训练参数

--- ultralytics-main/train_v8.py
+++ ultralytics-main/train_v8.py
@@ -12,15 +12,16 @@ def main(opt):
 #    yaml = opt.cfg
 #    model = YOLO('ultralytics/cfg/models/v8/yolov8-dww.yaml') 
     model = YOLO('yolov8s.yaml') 
-   
-#    model = YOLO('ultralytics/cfg/models/v8/yolov8-CustomConvAttentionBlock.yaml') 
-#    model = YOLO('ultralytics/cfg/models/v8/yolov8-MobileViTxs.yaml') 
+
     results = model.train(data='yolo_du_seeship.yaml', 
                         epochs=100, 
                         batch=64,
 #                        device=[0,1],  # 指定GPU索引列表
 #                        workers=0,
                         imgsz=320,
+                        project='./my_train_logs',
+                        name='exp',  # 或其他名称
+                        exist_ok=False  # 如果目录已存在则报错（避免覆盖）
                         )
 
 def parse_opt(known=False):
